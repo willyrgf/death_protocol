b23a344a599b07f8fcef512e53db64d5bdec00de
willyrgf <willyr.goncalves@gmail.com>
2021-09-16 23:12:51 -0300

    add a nice logging

--- src/healthchecks/telegram/checkers.rs
+++ src/healthchecks/telegram/checkers.rs
@@ -2,7 +2,6 @@ use log::debug;
 use teloxide::prelude::*;
 
 pub async fn send_health_check() {
-    teloxide::enable_logging!();
     debug!("start bot...");
 
     let bot = Bot::from_env().auto_send();
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+extern crate log;
+extern crate pretty_env_logger;
+
 use death_protocol::start_checkers;
 use log::debug;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     debug!("start main()");
 
     start_checkers().await;
