e3bda647ed75848d1414bdf0c5f39ca1cb087a56
willyrgf <willyr.goncalves@gmail.com>
2021-09-17 22:34:14 -0300

    add stateful dialogue to the bot

--- src/healthchecks/telegram/checkers/mod.rs
+++ src/healthchecks/telegram/checkers/mod.rs
@@ -1,14 +1,30 @@
 use log::debug;
 use teloxide::prelude::*;
 
+use super::dialogue::Dialogue;
+
 pub async fn send_health_check() {
     debug!("start bot...");
 
     let bot = Bot::from_env().auto_send();
 
-    teloxide::repl(bot, |message| async move {
-        message.answer("hi").await?;
-        respond(())
+    teloxide::dialogues_repl(bot, |message, dialogue| async move {
+        handle_message(message, dialogue)
+            .await
+            .expect("something wrong with the bot.")
     })
     .await
+}
+
+async fn handle_message(
+    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    dialogue: Dialogue,
+) -> TransitionOut<Dialogue> {
+    match cx.update.text().map(ToOwned::to_owned) {
+        Some(ans) => dialogue.react(cx, ans).await,
+        None => {
+            cx.answer("send me a text message").await?;
+            next(dialogue)
+        }
+    }
 }
